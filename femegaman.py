56556f2783fed4f736a6badc546a7350bbc8c182
gtortone <gennaro.tortone@na.infn.it>
2020-06-01 20:22:40 +0200

    refactoring of event statistics

--- femegaman.py
+++ femegaman.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
 import midas
 import midas.frontend
@@ -6,12 +6,15 @@ import midas.event
 import sys
 import struct
 import zmq
+import zerorpc
 import lib.evutils
 from array import array
 
-equip_stats_settings = {"Events with EC non consistent": 0, "Events with CRC error": 0, "Events with LEN error": 0, "Events too short": 0}
+### params
+proxy_host = "lxconga01.na.infn.it"
+###
 
-class MegampManagerEquipment(midas.frontend.EquipmentBase):
+class MegampManagerEquipmentData(midas.frontend.EquipmentBase):
     def __init__(self, client, equip_name):
 
         default_common = midas.frontend.InitialEquipmentCommon()
@@ -23,14 +26,16 @@ class MegampManagerEquipment(midas.frontend.EquipmentBase):
         default_common.read_when = midas.RO_RUNNING
         default_common.log_history = 1
 
-        self.client = midas.client.MidasClient
-        self.odb_stats_base = "/Equipment/%s/Settings/" % equip_name
-
         self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.SUB)
-        self.socket.subscribe("")
-        self.socket.connect ("tcp://lxconga01.na.infn.it:5000")
-        
+        self.data_socket = self.context.socket(zmq.SUB)
+        self.data_socket.subscribe("")
+        ep_data = "tcp://%s:5000" % proxy_host
+        self.data_socket.connect(ep_data)
+
+        self.rpcclient = zerorpc.Client()
+        ep_ctrl = "tcp://%s:4242" % proxy_host
+        self.rpcclient.connect(ep_ctrl)
+
         self.eu = lib.evutils.EventUtils()
         self.eu.SetDebug(False)
         #
@@ -41,22 +46,29 @@ class MegampManagerEquipment(midas.frontend.EquipmentBase):
         self.buf = array('H')
         self.buflen = 0
 
-        midas.frontend.EquipmentBase.__init__(self, client, equip_name, default_common, (equip_stats_settings))
+        self.default_settings = {"Number of samples per channel": 0}
+
+        self.odb_stats_base = "/Equipment/%s/Counters" % equip_name
+        self.stats_settings = {"Events with CRC error": 0, "Events with LEN error": 0, "Events too short": 0, "Events with EC not consistent": 0}
+        client.odb_set(self.odb_stats_base, self.stats_settings)
+
+        midas.frontend.EquipmentBase.__init__(self, client, equip_name, default_common, self.default_settings)
+
         self.set_status("Initialized", "yellowLight")
 
     def begin_of_run(self, run_number):
         # reset event error statistics
-        self.settings["Events with EC non consistent"] = 0
-        self.settings["Events with CRC error"] = 0
-        self.settings["Events with LEN error"] = 0
-        self.settings["Events too short"] = 0
-        self.client.odb_set(self.odb_stats_base, self.settings)
+        self.stats_settings["Events with EC not consistent"] = 0
+        self.stats_settings["Events with CRC error"] = 0
+        self.stats_settings["Events with LEN error"] = 0
+        self.stats_settings["Events too short"] = 0
+        self.client.odb_set(self.odb_stats_base, self.stats_settings)
 
     def end_of_run(self, run_number):
         None
 
     def poll_func(self):
-        string = self.socket.recv()
+        string = self.data_socket.recv()
         self.buflen = len(string) // 2
         self.buf = struct.unpack('H'*self.buflen, string)
 
@@ -68,21 +80,21 @@ class MegampManagerEquipment(midas.frontend.EquipmentBase):
             if( (word & 0xF000) == 0xF000):      # end of event
                 event_error = False
                 if(len(self.event_raw) < 3):
-                    self.settings["Events with LEN error"] += 1
+                    self.stats_settings["Events with LEN error"] += 1
                     event_error = True
                 #self.eu.PrintTab(self.event_raw)
                 if (not self.eu.CheckEC(self.event_raw)):
-                    self.settings["Events with EC non consistent"] += 1
+                    self.stats_settings["Events with EC not consistent"] += 1
                     event_error = True
                 if (not self.eu.CheckCRC(self.event_raw)):
-                    self.settings["Events with CRC error"] += 1
+                    self.stats_settings["Events with CRC error"] += 1
                     event_error = True
                 if (not self.eu.CheckLength(self.event_raw)):
-                    self.settings["Events with LEN error"] += 1
+                    self.stats_settings["Events with LEN error"] += 1
                     event_error = True
 
                 if event_error is True:
-                    self.client.odb_set(self.odb_stats_base, self.settings)
+                    self.client.odb_set(self.odb_stats_base, self.stats_settings)
                     del self.event_raw[:]
                     return False
                 else:
@@ -114,8 +126,8 @@ class Frontend(midas.frontend.FrontendBase):
             print("Error: please specify frontend index (-i argument)")
             sys.exit(-1)
 
-        self.equip_name = "MegampManager%s" % str(midas.frontend.frontend_index).zfill(2)
-        self.mmeq = MegampManagerEquipment(self.client, self.equip_name)
+        self.equip_name = "MegampManager%s-data" % str(midas.frontend.frontend_index).zfill(2)
+        self.mmeq = MegampManagerEquipmentData(self.client, self.equip_name)
         self.add_equipment(self.mmeq)
         if(self.run_state == midas.STATE_RUNNING):
             self.set_all_equipment_status("Running", "greenLight")
@@ -125,13 +137,13 @@ class Frontend(midas.frontend.FrontendBase):
         self.client.msg("Frontend has seen start of run number %d" % run_number)
         self.mmeq.begin_of_run(run_number)
         return midas.status_codes["SUCCESS"]
-        
+
     def end_of_run(self, run_number):
         self.set_all_equipment_status("Finished", "greenLight")
         self.client.msg("Frontend has seen end of run number %d" % run_number)
         self.mmeq.end_of_run(run_number)
         return midas.status_codes["SUCCESS"]
-        
+
 if __name__ == "__main__":
     fe = Frontend()
     fe.run()
