0e6a3a9a0de347a45745702e7172d3b4b3849be2
gtortone <gennaro.tortone@na.infn.it>
2020-06-04 08:55:40 +0200

    check megaman-usb-proxy with XML-RPC supervisord

--- femegaman.py
+++ femegaman.py
@@ -12,9 +12,12 @@ from lib.register import GetRegAddress, GetRegs,\
         GetCommitRegs, BOR, SOF, SCLR, SCAN
 from array import array
 from time import sleep
+from xmlrpc.client import ServerProxy
 
 ### params
 proxy_host = "lxconga01.na.infn.it"
+proxy_user = "midas"
+proxy_pass = "m1d4s"
 ###
 
 class MegampManagerEquipmentCtrl(midas.frontend.EquipmentBase):
@@ -218,11 +221,43 @@ class MegampManagerEquipmentData(midas.frontend.EquipmentBase):
 
 class Frontend(midas.frontend.FrontendBase):
     def __init__(self):
+
         midas.frontend.FrontendBase.__init__(self, "MegaManFE")
         feindex = midas.frontend.frontend_index
         if(feindex == -1):
             print("Error: please specify frontend index (-i argument)")
             sys.exit(-1)
+
+        # check if megaman-usb-proxy is running
+        server = ServerProxy("http://%s:%s@%s:9001/RPC2" % (proxy_user,
+                                                            proxy_pass,
+                                                            proxy_host))
+
+
+        info = None
+        try:
+            info = server.supervisor.getProcessInfo('megaman-usb-proxy')
+        except:
+            pass
+
+        if(info is None):
+            self.client.msg("E: supervisor on proxy host is not running",
+                           is_error=True)
+            self.client.msg("check configuration of %s" % proxy_host)
+            sys.exit(-1)
+
+        if(info['statename'] != 'RUNNING'):
+            # try to restart megaman-usb-proxy
+            self.client.msg("I: megaman-usb-proxy is not running ")
+            self.client.msg("I: megaman-usb-proxy restarting")
+            server.supervisor.startProcess('megaman-usb-proxy')
+            info = server.supervisor.getProcessInfo('megaman-usb-proxy')
+            if(info['statename'] != 'RUNNING'):
+                self.client.msg("E: megaman-usb-proxy failed restart",
+                               is_error=True)
+                sys.exit(-1)
+            else:
+                self.client.msg("I: megaman-usb-proxy restarted successfully")
 
         equip_name = "MegampManager%s" % \
                 str(midas.frontend.frontend_index).zfill(2)
